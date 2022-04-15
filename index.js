ee6f030f05592d7ca20758fbeea9d7dc87e55008
Miguel Mendes <mbmiguel89@gmail.com>
2022-04-15 16:39:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const container =  document.querySelector(".wrapper-question");
+const question1 = document.querySelector(".question-1");
+const arrow = document.querySelector(".img-arrow")
+const answer = document.querySelector(".resp");
+
+
+const showAnswer1 = () => {
+    answer.classList.toggle("resp-show");
+    question1.classList.toggle("title-weight")
+    arrow.classList.toggle("transform")
+}
+
+const question2 = document.querySelector(".question-2");
+const arrow2 = document.querySelector(".img-arrow2")
+const answer2 = document.querySelector(".resp2");
+
+const showAnswer2 = () => {
+    answer2.classList.toggle("resp-show");
+    question2.classList.toggle("title-weight")
+    arrow2.classList.toggle("transform")
+}
+
+const question3 = document.querySelector(".question-3");
+const arrow3 = document.querySelector(".img-arrow3")
+const answer3 = document.querySelector(".resp3");
+
+const showAnswer3 = () => {
+    answer3.classList.toggle("resp-show");
+    question3.classList.toggle("title-weight")
+    arrow3.classList.toggle("transform")
+}
+
+const question4 = document.querySelector(".question-4");
+const arrow4 = document.querySelector(".img-arrow4")
+const answer4 = document.querySelector(".resp4");
+
+const showAnswer4 = () => {
+    answer4.classList.toggle("resp-show");
+    question4.classList.toggle("title-weight")
+    arrow4.classList.toggle("transform")
+}
+
+const question5 = document.querySelector(".question-5");
+const arrow5 = document.querySelector(".img-arrow5")
+const answer5 = document.querySelector(".resp5");
+
+const showAnswer5 = () => {
+    answer5.classList.toggle("resp-show");
+    question5.classList.toggle("title-weight")
+    arrow5.classList.toggle("transform")
+}
